5bbf677ecf591919bb4a9a44a05e3546abf82579
phyu4capstone <phyu4capstone@gmail.com>
2024-01-16 16:22:30 +0800

    added aws sam to deploy serverless fun

---
+++ createMint.mjs
@@ -0,0 +1,61 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  PutCommand
+} from "@aws-sdk/lib-dynamodb";
+
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString();
+};
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+
+  try {
+    const requestBody = JSON.parse(event.body);
+    const { name, description, image } = requestBody;
+
+    // Generate a random UUID using the uuid library
+    const uuid = generateRandomNumber();
+
+
+    // Put item in DynamoDB
+    await dynamo.send(
+          new PutCommand({
+            TableName: 'mintBE',
+            Item: {
+              mintId: uuid,
+              name: name,
+              description: description,
+              image : image ,
+            },
+          })
+        );
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Item mint successfully' }),
+    }
+    
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
---
+++ getMint.mjs
@@ -0,0 +1,68 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient();
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  };
+
+
+  try {
+
+    const uuid = event?.queryStringParameters?.uuid
+    const tableName = 'mintBE'
+    
+    let params = {
+      TableName: tableName,
+    }
+    if (uuid) {
+      params = {
+        ...params,
+        FilterExpression: '#myAttr = :uuidValue', 
+        ExpressionAttributeNames: {
+          '#myAttr': 'mintId', 
+        },
+        ExpressionAttributeValues: {
+          ':uuidValue': uuid, 
+        },
+      };
+    }
+    console.log('params', params)
+    // Create the ScanCommand
+    const command = new ScanCommand(params);
+    
+    // Put item in DynamoDB
+    const response = await dynamo.send(command);
+    console.log('response', JSON.stringify(response))
+    
+    body = response.Items
+    return {
+      statusCode: 200,
+      body
+    }
+    
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    return {
+      statusCode,
+      headers,
+      body: JSON.stringify(body)
+    };
+  }
+
+};
